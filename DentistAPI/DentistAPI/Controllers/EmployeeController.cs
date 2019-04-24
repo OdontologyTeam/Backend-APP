b118263beacc760d398201ff3827ab640c09dca7
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-04-24 13:46:28 -0500

    Adding POST and PUT methods

--- DentistAPI/DentistAPI/Controllers/EmployeeController.cs
+++ DentistAPI/DentistAPI/Controllers/EmployeeController.cs
@@ -9,11 +9,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DentistAPI.Controllers
 {
+    //Controlador para los llamados relacionados a los empleados o usuarios
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeeController : ControllerBase
     {
         private readonly dentistdbContext _dentistdbContext;
+        //Constructor que crea un usuario si no hay registros en la tabla
         public EmployeeController(dentistdbContext dentistdbContext)
         {
             _dentistdbContext = dentistdbContext;
@@ -23,11 +25,13 @@ namespace DentistAPI.Controllers
                 _dentistdbContext.SaveChanges();
             }
         }
+        //Listado de todos los usuarios llamado get https://localhost:44358/api/employee
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Usuario>>> GetEmployees()
         {
             return await _dentistdbContext.Usuario.ToListAsync();
         }
+        //consulta de un usuario llamado get https://localhost:44358/api/employee/cedula
         [HttpGet("{id}")]
         public async Task<ActionResult<Usuario>> GetEmployee(string id)
         {
@@ -39,6 +43,27 @@ namespace DentistAPI.Controllers
             }
 
             return usuario;
+        }
+        //insercion de un usuario llamado post https://localhost:44358/api/employee en el body del request se envia el json
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> AddEmployee(Usuario usuario)
+        {
+            _dentistdbContext.Usuario.Add(usuario);
+            await _dentistdbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployee), new { id = usuario.Cedula }, usuario);
+        }
+        //actualizacion de un usuario llamado put https://localhost:44358/api/employee/cedula en el body del request se envia el json
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(string id, Usuario usuario)
+        {
+            if (!id.Equals(usuario.Cedula))
+            {
+                return BadRequest();
+            }
+
+            _dentistdbContext.Entry(usuario).State = EntityState.Modified;
+            await _dentistdbContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
